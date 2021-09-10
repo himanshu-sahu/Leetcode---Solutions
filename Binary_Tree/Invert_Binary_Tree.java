0111274551a45670dd648d659ec47533bca9dafc
himanshu-sahu <83325706+himanshu-sahu@users.noreply.github.com>
2021-09-10 18:27:50 +0530

    Create Invert_Binary_Tree.java

---
+++ Binary_Tree/Invert_Binary_Tree.java
@@ -0,0 +1,14 @@
+Que ->Given the root of a binary tree, invert the tree, and return its root.
+
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) return null;
+        invertTree(root.left);
+        invertTree(root.right);
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        return root;
+    }
+ }
